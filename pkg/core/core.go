7ba0ba27992ad51c236f93ab7d03cb82da69b2b9
Baihua Chen <bchen102@myseneca.ca>
2025-09-26 00:03:06 -0400

    feat: add last modified timestamp to file info
    - added ModTime to FileInfo struct
    - show ModTime in formatter

--- pkg/core/core.go
+++ pkg/core/core.go
@@ -159,6 +159,7 @@ func processFile(filePath string, outputFile string, displayLineNum bool) error 
 				IsDir:        false,
 				Size:         stat.Size(),
 				Content:      content,
+				ModTime:      stat.ModTime(),
 				Error:        nil,
 			},
 		},

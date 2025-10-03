ee8ccfa85a5130a5b0bff94c08ee61d20dd21486
Cynthia Fotso <cynthiafotso8@gmail.com>
2025-10-03 02:52:59 -0400

    removed commented-out fields from FlagConfig struct

--- pkg/flagConfig/flagConfig.go
+++ pkg/flagConfig/flagConfig.go
@@ -2,11 +2,6 @@ package flagConfig
 
 // FlagConfig stores configuration options
 type FlagConfig struct {
-	/*ConfigFile    string
-	NoGitignore   bool
-	OutputFile    string
-	DisplayLineNum bool
-	Verbose       bool*/
 	ConfigFile     string `mapstructure:"config"`
 	NoGitignore    bool   `mapstructure:"no_gitignore"`
 	OutputFile     string `mapstructure:"output"`

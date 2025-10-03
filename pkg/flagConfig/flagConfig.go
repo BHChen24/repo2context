1fa894ed3c77501cfd43df65b1f583e418eb0837
Cynthia Fotso <cynthiafotso8@gmail.com>
2025-10-03 02:50:43 -0400

    refactor: update FlagConfig struct to use struct tags for configuration options

--- pkg/flagConfig/flagConfig.go
+++ pkg/flagConfig/flagConfig.go
@@ -2,9 +2,14 @@ package flagConfig
 
 // FlagConfig stores configuration options
 type FlagConfig struct {
-	ConfigFile    string
+	/*ConfigFile    string
 	NoGitignore   bool
 	OutputFile    string
 	DisplayLineNum bool
-	Verbose       bool
+	Verbose       bool*/
+	ConfigFile     string `mapstructure:"config"`
+	NoGitignore    bool   `mapstructure:"no_gitignore"`
+	OutputFile     string `mapstructure:"output"`
+	DisplayLineNum bool   `mapstructure:"display_line_num"`
+	Verbose        bool   `mapstructure:"verbose"`
 }
